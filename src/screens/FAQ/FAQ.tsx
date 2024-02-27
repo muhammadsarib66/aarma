053769b7cfb80da4749863a1bb1746606960e02d
muhammadsarib66 <saribnoor0310@gmail.com>
2024-02-27 19:39:54 +0500

    fix

--- src/screens/FAQ/FAQ.tsx
+++ src/screens/FAQ/FAQ.tsx
@@ -1,37 +1,60 @@
-import Accordion from '@mui/material/Accordion';
-import AccordionSummary from '@mui/material/AccordionSummary';
-import AccordionDetails from '@mui/material/AccordionDetails';
-import Typography from '@mui/material/Typography';
-import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import Accordion from "@mui/material/Accordion";
+import AccordionSummary from "@mui/material/AccordionSummary";
+import AccordionDetails from "@mui/material/AccordionDetails";
+import Typography from "@mui/material/Typography";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+import { useState } from "react";
 
 const FAQ = () => {
-  return (
-    <section className='h-[80vh] bg-secondary flex flex-col py-20 gap-12 items-center'>
-      <div>
-      <h2 className="text-2xl md:text-3xl text-onPrimary font-semibold text-center pt-8">Frequently Asked Questions</h2>
+  // const [expendedd, setExpanded] = useState(false)
 
+  // const handleExpansion = () => {
+  //   setExpanded((prevExpanded) => !prevExpanded);
+  // };
+  const [expanded, setExpanded] = useState<any | false>(false);
+
+  const handleChange =
+    (panel: any) => (event: React.SyntheticEvent, isExpanded: boolean) => {
+      setExpanded(isExpanded ? panel : false);
+    };
+  return (
+    <section className="h-[80vh] bg-onSecondary flex flex-col py-20 gap-12 items-center">
+      <div>
+        <h2 className="text-2xl md:text-3xl text-onPrimary font-semibold text-center pt-8">
+          Frequently Asked Questions
+        </h2>
       </div>
-    <div className='w-[70%]' >
-      <Accordion defaultExpanded>
-        <AccordionSummary
-          expandIcon={<ExpandMoreIcon />}
-          aria-controls="panel1-content"
-          id="panel1-header"
-        >
-          <Typography>Expanded by default</Typography>
-        </AccordionSummary>
-        <AccordionDetails>
-          <Typography>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
-            malesuada lacus ex, sit amet blandit leo lobortis eget.
-          </Typography>
-        </AccordionDetails>
-      </Accordion>
-     
-    </div>
+      <div className="w-[90%] md:w-[60%] ">
+        {[1, 2, 3, 4].map((item, ind) => (
+         
+          <Accordion
+            key={ind}  
+            sx={{marginBottom: '20px'}}
+            expanded={expanded === item}
+            onChange={handleChange(item)}
+          >
+            <AccordionSummary
+              expandIcon={<ExpandMoreIcon />}
+              aria-controls="panel1bh-content"
+              id="panel1bh-header"
+            >
+              <h3 className=" text-lg md:text-xl font-semibold text-onPrimary" >
+               Q:{ind+1 }  Why do I partner with AARMA
+              </h3>
+            </AccordionSummary>
+            <AccordionDetails>
+              <p className="text-placeHolder font-semibold px-2">
+                AARMA helps businesses grow their sales, reach new customers,
+                and increase brand awareness online. By partnering with us, you
+                can get access to new customers who want to order from
+                businesses like yours for delivery and pickup.
+              </p>
+            </AccordionDetails>
+          </Accordion>
+        ))}
+      </div>
     </section>
-  
   );
-}
+};
 
 export default FAQ;
