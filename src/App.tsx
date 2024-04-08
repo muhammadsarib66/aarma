170a6a31310d56d23265d6b65b81cd4c5ded7019
muhammadsarib66 <saribnoor0310@gmail.com>
2024-04-08 17:05:35 +0500

    chnagses

--- src/App.tsx
+++ src/App.tsx
@@ -7,30 +7,34 @@ import Home from "./screens/Home/Home";
 import { Routes, Route } from "react-router-dom";
 import "react-toastify/dist/ReactToastify.css";
 import Login from "./screens/Login/Login";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { useEffect } from "react";
 import { getAllCatApi } from "./features/slicer/CategorySlicer";
 import { GetMyProfile } from "./features/slicer/GetMyProfileSlicer";
-import Dashboard2 from "./screens/Dashboard2/Dashboard2";
+import Dashboard2 from "./screens/Dashboard2/Dashboard2"
 import { ToastContainer } from "react-toastify";
 function App() {
   const dispatch = useDispatch();
 
-  // const { UserData } = useSelector((state: any) => state.LoginSlicer);
-  // console.log(UserData, "UserData")
+  const { UserData } = useSelector((state: any) => state.LoginSlicer);
+  console.log(UserData, "UserData")
   // const { ProfileData } = useSelector((state: any) => state.GetMyProfileSlicer);
-
+  
   const token = localStorage.getItem("token");
 
   useEffect(() => {
+
     if (token) {
       try {
         // Automatically log in with saved credentials
+        // dispatch(LoginAccApi(parsedCredentials));
+        // dispatch(LoginAccApi(UserData))
+        // Navigate to "/Operations2"
         dispatch<any>(getAllCatApi());
         dispatch(GetMyProfile());
         // navigate("/Dashboard");
       } catch (error: any) {
-        // console.error("Error parsing saved credentials:", error.message);
+        console.error("Error parsing saved credentials:", error.message);
       }
     }
   }, []);
@@ -38,7 +42,8 @@ function App() {
   return (
     <>
       <>
-        <Navbar />
+        {/* <Navbar /> */}
+          <Navbar />
         <ScrollToTop />
         <Routes>
           <>
@@ -50,14 +55,25 @@ function App() {
                 <Route path="/login" element={<Login />} />
               </>
             )}
+            {/* {token && (
+              <><Route path ="/dashboard2/*" element={<Dashboard2 />} />
+                <Route path="/*" element={<Dashboard />} />
+                <Route path="/Dashboard" element={<Dashboard />} />
+                <Route path="/Analytics" element={<Analatics />} />
+                <Route path="/Bookings" element={<Bookings />} />
+                <Route path="/myprofile" element={<MyProfile />} />
+              </>
+            )} */}
           </>
         </Routes>
-        {token && (
-          <>
-            <Dashboard2 />
-            <ToastContainer />
-          </>
-        )}
+            {
+              token && 
+              <>
+              <Dashboard2 />
+      <ToastContainer />
+              </>
+            }
+
       </>
     </>
   );
